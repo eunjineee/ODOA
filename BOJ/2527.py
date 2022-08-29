e2d59828ffa2d012089f0368d2a57c7aff7b3367
eunjin <yej6642@gmail.com>
2022-08-29 23:48:32 +0900

    0829

--- BOJ/2527.py
+++ BOJ/2527.py
@@ -3,23 +3,15 @@ for _ in range(4):
     aa = [(a,b),(a,d),(c,b),(c,d)]
     bb = [(e,f),(e,h),(g,f),(g,h)]
 
-    for q in aa:
-            for w in bb:
-                if q == w:
-                    print('c')
-                    continue
-    xx = [a,c,e,g]
-    yy = [b,d,f,h]
-    cnt = 0
-    for x in xx:
-        for y in yy:
-            if x == y:
-                cnt += 1
-    if cnt == 1:
-        print('b')
-        continue    
-    elif b < f < d or b < h < d or a < e < c or a < g < c:
-        print('a')
-        
-    else:
+    if e > c or d < f or g < a or b > h:
         print('d')
+    elif (
+        (a == g and f == d) or
+        (c == e and d == f) or
+        (c == e and b == h) or
+        (g == a and h == b)):
+        print('c')
+    elif c == e or b == h or a == g or d == f:
+        print('b')    
+    else:
+        print('a')
