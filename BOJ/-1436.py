5fdd57f6babbec0c848f9602c0f03d76114f62fb
goeom77 <7eom14@gmail.com>
2022-09-15 17:38:44 +0900

    0915

--- BOJ/-1436.py
+++ BOJ/-1436.py
@@ -1,8 +1,15 @@
 n = int(input())
 a = 666
 
-b = (n-13) % 24
+b = (n-13) // 24 + 1
 c = n % 24
-if 13 >= c >= 7:
-    num = b*1000 + a
-elif 
+print(b,c)
+
+if 6 >= c >= 0:
+    num = b*10000 + (c-1)*1000 + a
+elif 13 >= c >= 7:
+    num = b*10000 + a*10 + (c-7)
+elif 23 >= c >= 14:
+    num = (b-2)*10000 + (c-13)*1000 + a
+
+print(num)
---
+++ BOJ/-2108.py
@@ -0,0 +1,6 @@
+n = int(input())
+
+li = list(map(int, input().split()))
+li.sort()
+sumli = sum(li)
+
