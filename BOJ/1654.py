12627a68e50e26cab94f6d0dec26ea063619dc99
eunjineee <yej6642@gmail.com>
2022-09-20 00:38:03 +0900

    0920

--- BOJ/1654.py
+++ BOJ/1654.py
@@ -1,19 +1,43 @@
-import heapq
 k, n = map(int,input().split())
-nums = heapq()
-
+nums = []
 for _ in range(k):
-    nums.heapqushint(input()))
+    nums.append(int(input()))
 
+start = 1
+end = max(nums)
+
+while start <= end:
+    mid = (start + end)//2
+    b = 0
+    # print(f'{start} {mid} {end}')
+    for j in nums:
+        b += j//mid
+    if b >= n:
+        start = mid + 1
+        # print(f'커{b} {start} {mid} {end}')
+    else:
+        end = mid - 1
+        # print(f'작아{b} {start} {mid} {end}')
+print(end)
+
+# print(f'{start} {number} {mid}')
+
+'''
+k, n = map(int,input().split())
+nums = []
+for _ in range(k):
+    nums.append(int(input()))
 numbers = nums[:]
+nums.sort()
 
 while len(nums) < n:
-    a = nums.leftpop()[1]//2
+    a = nums.pop()//2
     b = 0
     for j in numbers:
         b += j//a
     if b >= n:
         print(a)
         break
-    nums.heapqush(nums, (-a,a))
-    
+    nums.append(a)
+    nums.sort()
+    '''
