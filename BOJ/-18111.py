8eaf94f2a14d508a02f5246190ccc0142f4ebfda
eunjineee <yej6642@gmail.com>
2022-09-24 20:18:56 +0900

    0924

--- BOJ/-18111.py
+++ BOJ/-18111.py
@@ -1,11 +1,32 @@
-n,m,b = map(int, input().split())
+import sys
+sys.stdin = open('input.txt')
 
-nums = []
-totala = 0
+n, m, bag = map(int, input().split())
+
+ground = []
+
 for _ in range(n):
-    a = list(map(int, input().split()))
-    nums += a
-    totala += sum(a)
+    ground += list(map(int, input().split()))
+ground.sort()
 
-while totala//(n*m) != nums[-1]:
-    
+subbag1 = subbag2 = 0
+middle = round(sum(ground) // (n*m))
+for i in ground:
+    if middle > i:
+        subbag1 += (middle - i)
+    elif i > middle:
+        subbag2 += i - middle
+
+ans = 0
+while ground[0] != ground[-1]:
+    if subbag2 <= subbag1*2:
+        ground.append(ground.pop(0)+1)
+        bag -= 1
+        ground.sort()
+        ans += 1
+    else:
+        ground.append(ground.pop()-1)
+        bag -= 1
+        ground.sort()
+        ans += 2
+print(ans,ground[0])
