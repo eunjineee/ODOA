57cfebe6d8948833e6645cfceeb3300d4d749114
EUNJIN <yej6642@naver.com>
2022-10-31 14:50:26 +0900

    1031

--- PGS/118667 두 큐 합 같게 만들기.py
+++ PGS/118667 두 큐 합 같게 만들기.py
@@ -1,11 +1,36 @@
-from itertools import combinations 
+from collections import deque
 
 def solution(queue1, queue2):
-    total = queue1 + queue2
-    totals = []
-    for i in range(1,len(total)+1):
-        totals += list(combinations(total, i))
-    print(totals)
+    q1 = deque(queue1)
+    q2 = deque(queue2)
+    q1.reverse()
+    q2.reverse()
+    if sum(q1) == sum(q2):
+        num = 1
+    else:
+        num = 0
+        ans = []
+        while q1:
+            a = q1.popleft()
+            q2.append(a)
+            num += 1
+            if sum(q1) == sum(q2):
+                ans.append(num)
+
+        q1 = deque(queue1)
+        q2 = deque(queue2)
+        q1.reverse()
+        q2.reverse()
+        num = 0
+        while q2:
+            a = q2.popleft()
+            q1.append(a)
+            num += 1
+            if sum(q2) == sum(q1):
+                ans.append(num)
+        print(ans)
+                
+    
     answer = -2
     return answer
 
