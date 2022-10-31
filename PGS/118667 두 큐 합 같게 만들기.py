a0395454c138b373faf67fa25f62deae0fb5f6c8
EUNJIN <yej6642@naver.com>
2022-10-31 17:56:28 +0900

    1031

--- PGS/118667 두 큐 합 같게 만들기.py
+++ PGS/118667 두 큐 합 같게 만들기.py
@@ -3,10 +3,15 @@ from collections import deque
 def solution(queue1, queue2):
     q1 = deque(queue1)
     q2 = deque(queue2)
-    q1.reverse()
-    q2.reverse()
+    # q1.reverse()
+    # q2.everse()
+    if (sum(q1) + sum(q2)) // 2 == 1:
+        answer = -1
+        return answer
+
     if sum(q1) == sum(q2):
-        num = 1
+        answer = 1
+        return answer
     else:
         num = 0
         ans = []
@@ -16,22 +21,31 @@ def solution(queue1, queue2):
             num += 1
             if sum(q1) == sum(q2):
                 ans.append(num)
-
-        q1 = deque(queue1)
-        q2 = deque(queue2)
-        q1.reverse()
-        q2.reverse()
-        num = 0
-        while q2:
-            a = q2.popleft()
-            q1.append(a)
+            b = q2.popleft()
+            q1.append(b)
             num += 1
-            if sum(q2) == sum(q1):
+            if sum(q1) == sum(q2):
                 ans.append(num)
+            if num > len(queue1)*2 + 1:
+                break
+
+        # q1 = deque(queue1)
+        # q2 = deque(queue2)
+        # # q1.reverse()
+        # # q2.reverse()
+        # num = 0
+        # while q2:
+        #     a = q2.popleft()
+        #     q1.append(a)
+        #     num += 1
+        #     if sum(q2) == sum(q1):
+        #         ans.append(num)
         print(ans)
                 
     
-    answer = -2
-    return answer
+        answer = min(ans)
+        return answer
 
 solution([3, 2, 7, 2], [4, 6, 5, 1])
+solution([1, 2, 1, 2], [1, 10, 1, 2])
+solution([1, 1], [1, 5])
